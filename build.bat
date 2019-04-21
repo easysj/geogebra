94c4ffb8c8e71e7d288ba0fe4522bd81420df6ed
zhang <zhang@zms.me>
2019-04-22 02:36:45 +0800

    build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 
-@set JAVA_HOME=C:\Users\zhang\Desktop\123\jdk1.8.0_211
+@set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_211
 
-@path=C:\Users\zhang\Desktop\123\gradle-5.3.1\bin;%JAVA_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
+@path=%JAVA_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;
 
 cmd
 @echo gradlew assembleRelease
